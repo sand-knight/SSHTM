f79f4f394bc5ecf40a6e5757615c278d7d836bb0
Giulio <gaotar@gmail.com>
2022-05-05 23:27:31 +0200

    Script object IO

--- lib/Scripts/cubit_Scripts.dart
+++ lib/Scripts/cubit_Scripts.dart
@@ -1,3 +1,7 @@
+
+
+import 'dart:io';
+
 import "package:flutter_bloc/flutter_bloc.dart";
 import 'package:sshtm/Scripts/object_Script.dart';
 import 'package:sshtm/Scripts/state_Script.dart';
@@ -6,24 +10,35 @@ class cubit_Scripts extends Cubit<scriptsState> {
   /* Once again here is the "state", as the changing data structure */
   final ScriptList _localList;
 
-  cubit_Scripts(this._localList) : 
-  /* From source: super ( initial state )
+  cubit_Scripts(this._localList)
+      :
+        /* From source: super ( initial state )
     could not, for the life of me, instanciate
     a new ScriptList in the constructor without
     a factory function, if that is a solution
   */
-                  super(scriptListNotLoadedState(_localList.list));
+        super(scriptListNotLoadedState(_localList.list));
 
   void loadScriptList() {
     _localList.load();
-    emit (scriptListLoadedState(_localList.list));
+    emit(scriptListLoadedState(_localList.list));
   }
 
   //TODO void add script from file
-
+  Future<bool> addScriptFromFile(List<File> toBeAdded) async {
+    bool strangeFile = await _localList.addScriptFromFile(toBeAdded);
+    emit(scriptListLoadedState(_localList.list));
+    return strangeFile;
+  }
   //TODO void add script from text field
+  void addScriptFromText(String name, String content) async {
+    addScriptFromText(name, content);
+    emit(scriptListLoadedState(_localList.list));
+  }
 
   //TODO void remove script
-
-
+  void deleteScriptFromList(Script toDelete) {
+    _localList.deleteScript(toDelete);
+    emit(scriptListLoadedState(_localList.list));
+  }
 }
--- lib/Scripts/object_Script.dart
+++ lib/Scripts/object_Script.dart
@@ -4,65 +4,145 @@ import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart';
 
 class Script {
-
-  final String _pathname;
+  final File _file;
   final String _name;
 
-  Script (this._name, this._pathname);
+  Script(this._name, this._file);
 
-  Future<String> get content async{
-    try {
-      final File file = File(_pathname);
-      Future<String> content = file.readAsString();
-      return content;
-    } catch (couldnotreadfile) {
-      rethrow; // TODO i'm expecting this will cause an error mesage and the script will be dropped from the list
+  //GET
+  String get name => _name;
+  String get path => _file.path;
+
+  set name (String newName){
+    String newpath=_file.parent.path+name;
+    try{
+      
+      _file.rename(newpath);
+    } catch (e) {
+      print ("Could not rename script. Name was $newpath");
+      rethrow;
     }
   }
 
+  Future<String> get content async {
+    try {
+      //final File file = File(_pathname);
+      Future<String> content = _file.readAsString();
+      return content;
+    } catch (couldnotreadfile) {
+      print ("could not read file");
+      rethrow; // TODO i'm expecting this will cause an error mesage and the script will be dropped from the list
+    }
+  }
 }
 
 class ScriptList {
-
-  List<Script>_list = <Script>[];
+  List<Script> _list = <Script>[];
 
   List<Script> get list => _list;
 
-  void load() async{
+  late Directory _scripdir;
+
+  void load() async {
     /* Discovery this' app folder*/
-    final Directory appdata= await getApplicationDocumentsDirectory();
-    final Directory scriptfolder = Directory.fromRawPath( Uint8List.fromList(utf8.encode(appdata.path + "/Script")));
-    
-    if (!( await scriptfolder.exists() )){
+    final Directory appdata = await getApplicationDocumentsDirectory();
+    _scripdir = Directory.fromRawPath(
+        Uint8List.fromList(utf8.encode(appdata.path + "/Script")));
+
+    if (!(await _scripdir.exists())) {
       /* folders does not exist: create it and exit, because it's empty*/
       try {
-        scriptfolder.create();
+        print("Scripdir not found: creating...");
+        _scripdir.create();
         return;
-      } catch (couldnotcreatefolder){
+      } catch (couldnotcreatefolder) {
+        String incriminedPath=_scripdir.path;
+        print("Failed to create scripdir of path $incriminedPath");
         rethrow;
       }
     }
 
-
     List<FileSystemEntity> directorylist;
     /*the folder exists*/
-    try{
-      directorylist = scriptfolder.listSync();
+    try {
+      directorylist = _scripdir.listSync();
     } catch (couldnotreadfoldercontent) {
+      String incriminedPath=_scripdir.path;
+      print("Failed to load directory content at $incriminedPath");
       rethrow;
     }
-    
-    for (FileSystemEntity element in directorylist) {
-        if(element is File){
-          _list.add(Script(element.path.split('/').last, element.path));
-        }
-    }
 
+    for (FileSystemEntity element in directorylist) {
+      if (element is File) {
+        _list.add(Script(element.path.split('/').last, element));
+      }
+    }
   }
 
   //TODO Add from file
+  Future<bool> addScriptFromFile(List<File> toBeAdded) async {
+    bool strangeFile = false;
+    String shebang = "!#/bin/";
+    for (File f in toBeAdded) {
+      
+      /*try to get a glimpse of the file*/
+      try {
+        if (!(await f.readAsString()).startsWith(shebang)) {
+          strangeFile = true;
+        }
+        
+      }catch (e){
+        print("Could not open file to evaluate");
+      } 
+
+      /*try to create new file*/
+      String name = f.path.split("/").last;
+      String newpath = _scripdir.path + name;
+      try {
+        File newfile = await f.copy(newpath);
+        _list.add(Script(name, newfile));
+      }catch (e) {
+
+        print("Could not open file to evaluate, at $newpath");
+        rethrow;
+      }
+    }
+
+    return strangeFile;
+  }
 
   //TODO Add from text field
+  void addScriptFromText(String name, String scriptContent) async {
+    String newpath=_scripdir.path + name;
+    try {
+      File newScript = File(newpath);
+      if (await newScript.exists()) {
+        throw FileAlreadyExistingException();
+      }
+      newScript.create();
+
+      newScript.writeAsString(scriptContent);
+
+      _list.add(Script(name, newScript));
+    } catch (f) {
+      if (f is FileAlreadyExistingException) print("File already exists :(");
+      else print("could not create file at $newpath");
+      rethrow;
+    }
+  }
 
   //TODO remove script
+  void deleteScript(Script toDelete) {
+    try {
+      //File(toDelete._pathname).delete();
+      toDelete._file.delete();
+      _list.remove(toDelete);
+    } catch (e) {
+      print("Failed to delete script");
+      rethrow;
+    }
+    
+  }
 }
+
+class FileAlreadyExistingException extends FileSystemException {}
